6d2b38e4165eac60a121d5e9363c8f00f67c9037
sarthu210 <sarthu102@gmail.com>
2024-08-03 00:37:43 +0530

    Added Skill Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import SliddingLine from "./components/SliddingLine";
+import SkillSection from "./components/SkillSection";
 export default function Home() {
   return (
     <div className="bg-[#252525]">
       <NavBar/>
       <HeroSection/>
       <SliddingLine/>
-      <HeroSection/>
+      <SkillSection/>
     </div>
   );
 }
